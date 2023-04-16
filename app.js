b051452f4711351d3be888ca815c6357b1a5c14b
Divyanshi <divyanshisharma0703@gmail.com>
2023-04-16 22:24:51 +0530

    adding react builds

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading=React.createElement('h2',{id:'heading'}, "hello world in react!")
 // console.log(heading)
 // const root= ReactDOM.createRoot(document.getElementById('root'))
 // const y= root.render(heading)
-
-const heading=React.createElement('div',{id:'heading'},[React.createElement('div',{id:'heading2'}, React.createElement('h2',{},'this is h2 tag' )), React.createElement('div',{id:'heading3'}, React.createElement('h2',{},'this is h2 tag' )) ] )
- const root= ReactDOM.createRoot(document.getElementById('root'))
- const y= root.render(heading)
+const heading = React.createElement('div', { id: 'heading' }, [React.createElement('div', { id: 'heading2' },
+    React.createElement('h2', {}, 'This is headache')),
+React.createElement('div', { id: 'heading3' },
+    React.createElement('h2', {}, 'not anymore, issues resolved'))])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
